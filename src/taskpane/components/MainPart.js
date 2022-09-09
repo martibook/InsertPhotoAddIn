b82728de5371574c2ea6f4a5aabccb0b862f41d1
martibook <martibook@microsoft.com>
2022-09-09 23:51:11 +0800

    Preview all UIs

--- src/taskpane/components/MainPart.js
+++ src/taskpane/components/MainPart.js
@@ -1,16 +1,25 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useState } from "react";
-// import PreviewPhoto from "./PreviewPhoto";
+import PreviewPhoto from "./PreviewPhoto";
 import classes from "./MainPart.module.css";
 import ScanQRCode from "./ScanQRCode";
 
 const MainPart = () => {
   const temp = "https://www.thesprucepets.com/cute-dog-breeds-we-can-t-get-enough-of-4589340";
   const [qrValue, setqrValue] = useState(temp);
+  const [isCodePage, setIsCodePage] = useState(true);
+
+  const changePage = () => {
+    setIsCodePage((prevState) => !prevState);
+  };
 
   return (
     <div className={classes["main_block"]}>
-      <ScanQRCode value={qrValue}></ScanQRCode>
+      {isCodePage && <ScanQRCode value={qrValue}></ScanQRCode>}
+      {!isCodePage && <PreviewPhoto></PreviewPhoto>}
+      <button className={classes["change_page"]} onClick={changePage}>
+        Change Page
+      </button>
     </div>
   );
 };
